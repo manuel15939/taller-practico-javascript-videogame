21005160ab1521d6a799d6d98bfb0ca031726bfb
manuel15939 <manuel15939@gmail.com>
2023-05-22 13:22:21 -0500

    se dio estilos a objetos que estaran dentro de canvas clase 2

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-console.log(maps);
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
+
+window.addEventListener('load',startGame);
+
+function startGame(){
+    game.font = '25px Verdana';
+    game.fillStyle = 'purple';
+    game.textAling = 'end';
+    game.fillText ('Platzi',25,25);
+}
