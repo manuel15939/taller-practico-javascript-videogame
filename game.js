c179aeea1db1a7eba7e555eef511f76ca1181f51
manuel15939 <manuel15939@gmail.com>
2023-05-22 15:59:58 -0500

    se dio tamaño al canvas y sus elementos

--- game.js
+++ game.js
@@ -4,8 +4,25 @@ const game = canvas.getContext('2d');
 window.addEventListener('load',startGame);
 
 function startGame(){
-    game.font = '25px Verdana';
-    game.fillStyle = 'purple';
-    game.textAling = 'end';
-    game.fillText ('Platzi',25,25);
+    let canvasSize;
+
+    if (window.innerWidth < window.innerHeight){
+        canvasSize = window.innerWidth * 0.8
+    }else{
+        canvasSize = window.innerHeight * 0.8
+    }
+
+    canvas.setAttribute('width',  canvasSize);
+    canvas.setAttribute('height', canvasSize);
+
+    const elementSize = canvasSize/14;
+    console.log({canvasSize, elementSize});
+    game.font = elementSize +'px Verdana';
+    game.textAlign = 'end';
+    for (let i = 1; i <= 10 ; i++) {
+        game.fillText(emojis['X'], (canvasSize/10)*i, elementSize );
+        
+    }
+
+
 }
